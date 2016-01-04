70c3cfbafaf1200578eb1a324af6320c6153f3dc
Oliver Foster <oliver.foster@kineo.com>
2016-01-04 17:35:15 +0000

    Updated to ES6 classes, removed bind for ES6 arrow functions

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+"use strict";
+
 var GlobCollection = require("./lib/GlobCollection.js");
 var TreeContext = require("./lib/TreeContext.js");
 var MATCH_TYPE = require("./lib/MATCH_TYPE.js");
 
 module.exports = {
-	GlobCollection: GlobCollection,
-	TreeContext: TreeContext,
-	MATCH_TYPE: MATCH_TYPE,
+	GlobCollection,
+	TreeContext,
+	MATCH_TYPE,
 };
